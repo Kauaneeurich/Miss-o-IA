186e5ed6a54ed5947715b8e101e8ed8ed0c92995
Kauaneeurich <kauane.eurich.ribas@escola.pr.gov.br>
2026-09-24 10:00:47 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -5,16 +5,16 @@ const perguntas = [
             {
                 texto: "A preservação das florestas contribui para a proteção dos habitats naturais e para a conservação da biodiversidade",
                
-             afirmacao: "Guardião das Florestas Valoriza a preservação das florestas, reconhecendo sua importância para proteger os habitats, os animais, as plantas e a biodiversidade.",
+             afirmacao: ["Guardião das Florestas Valoriza a preservação das florestas, reconhecendo sua importância para proteger os habitats, os animais, as plantas e a biodiversidade.",
             "Protetor ambiental focado na conservação das florestas, garantindo o refúgio da fauna, da flora e de toda a biodiversidade."
-
+             ]
             },
         
             {
                 texto:  "A proteção dos rios e nascentes contribui para a conservação dos recursos hídricos e dos seres vivos que dependem deles.",
-                afirmacao: "💧 Guardião das Águas Valoriza a proteção dos rios e nascentes, reconhecendo que a água é essencial para a vida e para o equilíbrio dos ecossistemas.",
+                afirmacao: ["💧 Guardião das Águas Valoriza a proteção dos rios e nascentes, reconhecendo que a água é essencial para a vida e para o equilíbrio dos ecossistemas.",
                 "Protetor de rios e nascentes focado na segurança hídrica e ambiental do planeta."
-
+                ]
             }    
            
         ]
@@ -26,15 +26,15 @@ const perguntas = [
             alternativas: [
                 {
                     texto: "Reduzindo o consumo de água e energia.",
-                    afirmacao: " O(a) Economizador(a) Consciente:Pessoa que procura evitar desperdícios e utiliza água e energia de maneira responsável.",
+                    afirmacao:[ " O(a) Economizador(a) Consciente:Pessoa que procura evitar desperdícios e utiliza água e energia de maneira responsável.",
                   "Consumidor sustentável que combate o desperdício, utilizando água e energia de forma planejada e inteligente."
-
+                ]
                 },
                 {
                     texto:  "Reduzindo o uso de produtos descartáveis.",
-                    afirmacao: "O(a) Redutor(a) de Resíduos:Pessoa que prefere produtos reutilizáveis e busca produzir menos lixo no cotidiano.",
+                    afirmacao:[ "O(a) Redutor(a) de Resíduos:Pessoa que prefere produtos reutilizáveis e busca produzir menos lixo no cotidiano.",
                   "Consumidor sustentável que prioriza itens duráveis para diminuir a geração de lixo diário."
-
+                    ]
                 }
             ]
         },
@@ -45,16 +45,16 @@ const perguntas = [
             alternativas: [
                 {
                     texto: " Utilizaria bicicleta ou caminharia quando possível.",
-                    afirmacao: "O(a) Mobilizador(a) Sustentável:Pessoa que valoriza formas de transporte mais sustentáveis no dia a dia.",
+                    afirmacao: ["O(a) Mobilizador(a) Sustentável:Pessoa que valoriza formas de transporte mais sustentáveis no dia a dia.",
                   "Cidadão consciente que prioriza o transporte limpo, como a bicicleta e a caminhada, na rotina urbana."
-
+                    ]
                 }
                 {
                     texto:    "Utilizaria transporte coletivo.",
                
-                    afirmacao: " O(a) Usuário(a) Consciente:Pessoa que considera alternativas de transporte que podem diminuir o impacto ambiental.",
-                " Escolhe meios de transporte alternativos e eficientes para diminuir as emissões de carbono na rotina"
-
+                    afirmacao:[ " O(a) Usuário(a) Consciente:Pessoa que considera alternativas de transporte que podem diminuir o impacto ambiental.",
+                " Escolhe meios de transporte alternativos e eficientes para diminuir as emissões de carbono na rotina."
+                    ]
         }    
                
             ]
